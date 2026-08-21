0b0923f3eb23c08fa0095ce1721eeb7e81f713e5
rib <poleyrobert@gmail.com>
2026-08-21 10:52:38 -0400

    Updates to Javadoc

---
+++ src/DOCS/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"characters"},{"l":"dungeon"},{"l":"items"},{"l":"items.weapons"},{"l":"lib"},{"l":"ui"}];updateSearchResults();
